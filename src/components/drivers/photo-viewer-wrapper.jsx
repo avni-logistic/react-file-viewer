a026e805c54fd230414ae1e3b1b9ac493f04609d
Avni Raiyani <avni.raiyani@gmail.com>
2019-03-23 18:20:56 +0530

    changes

--- src/components/drivers/photo-viewer-wrapper.jsx
+++ src/components/drivers/photo-viewer-wrapper.jsx
@@ -52,10 +52,7 @@ export default class PhotoViewerWrapper extends Component {
   }
 
   componentWillReceiveProps(props) {
-    console.log('props', props);
-    console.log('this.props', this.props);
     if(props.filePath !== this.props.filePath) {
-      console.log('===>');
     // spike on using promises and a different loader or adding three js loading manager
       const loader = new THREE.TextureLoader();
       loader.crossOrigin = '';
@@ -83,7 +80,6 @@ export default class PhotoViewerWrapper extends Component {
   }
 
   render() {
-    console.log('state', state);
     if (!this.state.imageLoaded) {
       return <Loading />;
     }
